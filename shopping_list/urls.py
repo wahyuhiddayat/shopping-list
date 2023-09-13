329237e2f1b75808ef761f49fb3de76519eff3f6
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2023-09-13 14:14:35 +0700

    Tutorial 2

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,3 +1,5 @@
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+
 """
 URL configuration for shopping_list project.
 
@@ -19,6 +21,9 @@ from django.urls import path
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('json/', show_json, name='show_json'), 
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
